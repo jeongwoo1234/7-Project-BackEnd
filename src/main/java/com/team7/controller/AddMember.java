c35586d8cc44d84fee1653f2656c913efa383ed3
5pponent <ahdwjdtprtm@gmail.com>
2022-04-09 21:11:34 +0900

    단일 파일 업로드 구현

--- src/main/java/com/team7/controller/AddMember.java
+++ src/main/java/com/team7/controller/AddMember.java
@@ -1,6 +1,7 @@
 package com.team7.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,30 +9,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.team7.model.jsonParser;
 import com.team7.model.member.MemberDAO;
-import com.team7.model.member.MemberDTO;
 
-@WebServlet("/CreateMember")
-public class CreateMember extends HttpServlet {
+@WebServlet("/addMember")
+public class AddMember extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
+	private static final String ATTACHES_DIR = "C:\\attaches";
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// request json
-		request.setCharacterEncoding("UTF-8");
-		String jsonStr = jsonParser.parseJson(request);
-		Gson gson = new GsonBuilder().create();
-		MemberDTO member = gson.fromJson(jsonStr, MemberDTO.class);
 		
+		// json type request
+//		String jsonStr = Parser.parseStr(request);
+//		Gson gson = new GsonBuilder().create();
+//		MemberDTO member = gson.fromJson(jsonStr, MemberDTO.class);
+		
+		HashMap<String, String> member = Parser.parseMultipartToHashMap(request, response, ATTACHES_DIR);
 		MemberDAO memberDAO = new MemberDAO();
-		memberDAO.insertMember(member.getImage(), member.getName(), member.getBirthday(), member.getGender(), member.getJob());
-
+		memberDAO.insertMember(
+				member.get("image"), 
+				member.get("name"), 
+				member.get("birthday"), 
+				member.get("gender"), 
+				member.get("job")
+		);
+		
+		
 	}
-
 }
+
